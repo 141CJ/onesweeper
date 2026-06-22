d44f073b501e441ce6790dc506a375336e9cf931
141CJ <141cj+prod@proton.me>
2026-06-22 15:16:36 -0400

    feat: theme tile

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,30 @@ async fn main() {
     loop {
         clear_background(Color::from_hex(0xC0C0C0));
 
-        // bottom-right border of tile
-        draw_triangle(
-            vec2((screen_width() / 2.) - 10., (screen_height() / 2.) + 110.),
-            vec2((screen_width() / 2.) + 110., (screen_height() / 2.) + 110.),
-            vec2((screen_width() / 2.) + 110., (screen_height() / 2.) - 10.),
-            BLUE,
-        );
-
         // top-left border of tile
         draw_triangle(
             vec2((screen_width() / 2.) + 110., (screen_height() / 2.) - 10.),
             vec2((screen_width() / 2.) - 10., (screen_height() / 2.) - 10.),
             vec2((screen_width() / 2.) - 10., (screen_height() / 2.) + 110.),
-            GREEN,
+            Color::from_hex(0xFFFFFF),
+        );
+
+        // bottom-right border of tile
+        draw_triangle(
+            vec2((screen_width() / 2.) - 10., (screen_height() / 2.) + 110.),
+            vec2((screen_width() / 2.) + 110., (screen_height() / 2.) + 110.),
+            vec2((screen_width() / 2.) + 110., (screen_height() / 2.) - 10.),
+            Color::from_hex(0x7E7E7E),
         );
 
         // tile center
-        draw_rectangle(screen_width() / 2., screen_height() / 2., 100., 100., RED);
+        draw_rectangle(
+            screen_width() / 2.,
+            screen_height() / 2.,
+            100.,
+            100.,
+            Color::from_hex(0xC0C0C0),
+        );
 
         next_frame().await
     }
