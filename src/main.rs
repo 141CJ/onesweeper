b6ee5b0b229f890d25aed670209bf78a31dcf186
141CJ <141cj+prod@proton.me>
2026-06-22 15:12:38 -0400

    docs: specify which shape is what

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn main() {
     loop {
         clear_background(Color::from_hex(0xC0C0C0));
 
+        // bottom-right border of tile
         draw_triangle(
             vec2((screen_width() / 2.) - 10., (screen_height() / 2.) + 110.),
             vec2((screen_width() / 2.) + 110., (screen_height() / 2.) + 110.),
@@ -12,6 +13,7 @@ async fn main() {
             BLUE,
         );
 
+        // top-left border of tile
         draw_triangle(
             vec2((screen_width() / 2.) + 110., (screen_height() / 2.) - 10.),
             vec2((screen_width() / 2.) - 10., (screen_height() / 2.) - 10.),
@@ -19,6 +21,7 @@ async fn main() {
             GREEN,
         );
 
+        // tile center
         draw_rectangle(screen_width() / 2., screen_height() / 2., 100., 100., RED);
 
         next_frame().await
