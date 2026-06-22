1da99514ca2a9ad3bbaf8e7253efd378aab90d90
141CJ <141cj+prod@proton.me>
2026-06-22 15:32:57 -0400

    refactor: move tile drawing to draw_tile function

--- src/main.rs
+++ src/main.rs
@@ -6,49 +6,53 @@ async fn main() {
     loop {
         clear_background(Color::from_hex(0xC0C0C0));
 
-        // top-left border of tile
-        draw_triangle(
-            vec2(
-                (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-                (screen_height() / 2.) - (TILE_SIZE / 10.),
-            ),
-            vec2(
-                (screen_width() / 2.) - (TILE_SIZE / 10.),
-                (screen_height() / 2.) - (TILE_SIZE / 10.),
-            ),
-            vec2(
-                (screen_width() / 2.) - (TILE_SIZE / 10.),
-                (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-            ),
-            Color::from_hex(0xFFFFFF),
-        );
-
-        // bottom-right border of tile
-        draw_triangle(
-            vec2(
-                (screen_width() / 2.) - (TILE_SIZE / 10.),
-                (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-            ),
-            vec2(
-                (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-                (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-            ),
-            vec2(
-                (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
-                (screen_height() / 2.) - (TILE_SIZE / 10.),
-            ),
-            Color::from_hex(0x7E7E7E),
-        );
-
-        // tile center
-        draw_rectangle(
-            screen_width() / 2.,
-            screen_height() / 2.,
-            TILE_SIZE,
-            TILE_SIZE,
-            Color::from_hex(0xC0C0C0),
-        );
+        draw_tile();
 
         next_frame().await
     }
+}
+
+fn draw_tile() {
+    // top-left border of tile
+    draw_triangle(
+        vec2(
+            (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+            (screen_height() / 2.) - (TILE_SIZE / 10.),
+        ),
+        vec2(
+            (screen_width() / 2.) - (TILE_SIZE / 10.),
+            (screen_height() / 2.) - (TILE_SIZE / 10.),
+        ),
+        vec2(
+            (screen_width() / 2.) - (TILE_SIZE / 10.),
+            (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+        ),
+        Color::from_hex(0xFFFFFF),
+    );
+
+    // bottom-right border of tile
+    draw_triangle(
+        vec2(
+            (screen_width() / 2.) - (TILE_SIZE / 10.),
+            (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+        ),
+        vec2(
+            (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+            (screen_height() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+        ),
+        vec2(
+            (screen_width() / 2.) + TILE_SIZE + (TILE_SIZE / 10.),
+            (screen_height() / 2.) - (TILE_SIZE / 10.),
+        ),
+        Color::from_hex(0x7E7E7E),
+    );
+
+    // tile center
+    draw_rectangle(
+        screen_width() / 2.,
+        screen_height() / 2.,
+        TILE_SIZE,
+        TILE_SIZE,
+        Color::from_hex(0xC0C0C0),
+    );
 }
